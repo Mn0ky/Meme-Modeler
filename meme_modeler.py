1ab7cb22afadb8f2f14a00e6eacc254ef055f1b6
Bob Yule <whlittle5@gmail.com>
2023-10-07 16:45:12 -0400

    - Added individual plots for each meme curve. - Added plots for all meme curves of a certain type.
    - Both of the above can be found in the new "Plots" folder.

--- meme_modeler.py
+++ meme_modeler.py
@@ -1,18 +1,50 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-# Reads in data from a CSV
-aliens_data = pd.read_csv('Dataset\Ancient Aliens.csv', usecols = ['Month', 'Ancient Aliens: (United States)'],
-                          skiprows=2)
-#aliens_data.head() # TODO: Change this as it only reads first 5 rows of data!
 
-monthes = aliens_data.loc[:, 'Month'] # X-axis values, use iloc later instead of string value
-monthes = [month for month in range(len(monthes))] # Want monthes represent numerically, in order to fit on y-axis
-num_memes = aliens_data.loc[:, 'Ancient Aliens: (United States)'] # Y-axis values, use  iloc later instead of string value
+def main():
+    # File paths to all meme type folders
+    meme_types: list[str] = os.listdir('Dataset')
+    purge_ds_store(meme_types)
 
-plt.xlabel("Month")
-plt.ylabel("Number of Memes")
-plt.title("Number of Ancient Aliens Memes Over time") #
-plt.scatter(monthes, num_memes)
-plt.show()
+    # Want months represent numerically, in order to fit on y-axis
+    months: list[int] = [month for month in range(120)]  # All memes span the course of 120 months
+
+    for meme_type in meme_types:
+        meme_type_path = os.path.join('Dataset', meme_type)
+
+        meme_csvs = os.listdir(meme_type_path)
+        purge_ds_store(meme_csvs)
+        for meme_csv in meme_csvs:
+            meme_csv_path = os.path.join(meme_type_path, meme_csv)
+            plt.clf()
+            plt.title(f'Ranking of "{meme_csv}" Meme Over time')
+            plt.ylabel("Meme Ranking")
+            plt.xlabel("Month")
+
+            print("reading rankings for meme: " + meme_csv + " of type: " + meme_type)
+            meme_ranks: list[int] = read_meme_csv(meme_csv_path)  # Y-axis values
+
+            plt.plot(months, meme_ranks)
+            plot_path = os.path.join('Plots', meme_type, meme_csv)
+            plt.savefig(f'{plot_path}.png')
+
+
+# Returns the month-by-month ranks of a meme
+def read_meme_csv(csv_path: str) -> list[int]:
+    # Column 2 is meme rankings. Skip first 2 rows to get to start of column
+    rank_col = pd.read_csv(csv_path, usecols=[1], skiprows=2)
+    # Clean the ranking data to have only numeric values, then return them
+    return rank_col.replace('<1', 0).iloc[:, [0]].astype(int)
+
+
+def purge_ds_store(paths: list[str]):
+    for path in paths:
+        if path == '.DS_Store':
+            paths.remove(path)
+
+
+if __name__ == '__main__':
+    main()
